95c457639e58ccba466b332d18a31a1d436398c4
air_biker@abv.bg <air_biker@abv.bg@8db76cfa-2e72-11de-afe2-b5feb05456f9>
2009-04-23 15:48:27 +0000

    Merged the phx mesh viewer with the mesh viewer.

--- trunk/patch/source/Irrlicht/PhXRigidBody.cpp
+++ trunk/patch/source/Irrlicht/PhXRigidBody.cpp
@@ -35,16 +35,16 @@ void CPhXRigidBody::ApplyForce(const core::vector3df& force, const core::vector3
 
 void CPhXRigidBody::ApplyTorque(const core::vector3df& torque)
 {
-	core::quaternion q(torque.X,torque.Y,torque.Z, 1);
-	m_totalTorque = m_totalTorque + q;
-	//m_totalTorque += torque;
+	//core::quaternion q(torque.X,torque.Y,torque.Z, 1);
+	//m_totalTorque = m_totalTorque + q;
+	m_totalTorque += torque;
 }
 
 void CPhXRigidBody::UpdateVelocity(f32 step)
 {
 
 	m_linearVel += (step * ((f32)m_massInv)) * m_totalForce;
-	//m_algularVel += (step * m_massInv) * m_totalTorque;
+	m_algularVel += (step * m_massInv) * m_totalTorque;
 
 	m_totalForce.set(0,0,0);
 	m_totalTorque.set(0,0,0);
